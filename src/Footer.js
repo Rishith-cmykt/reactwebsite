d812fcd60443573e2bc5e1dd3ad683185638b9af
Rishith <70945517+Rishith-cmykt@users.noreply.github.com>
2022-04-08 04:19:02 -0500

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="bg-light"></footer>
+      <p className="text-center"> © 2020 Technical. All Rights Reserved | Terms and Conditions apply Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)</p>
+    </>
+  );
+};
+
+export default Footer;
